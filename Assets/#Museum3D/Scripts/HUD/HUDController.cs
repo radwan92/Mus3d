cbf4101e9d4b04b3802596abdbe7d6c23c20bed2
Filip Radwaniecki <filip.radwaniecki@gmail.com>
2017-03-15 20:46:18 +0100

    Attched sprite numbers to HUD and HUDController

---
+++ Assets/#Museum3D/Scripts/HUD/HUDController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mus3d
+{
+    public class HUDController : MonoBehaviour
+    {
+        [SerializeField] HUDNumber[] m_hudNubmers;
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void InitializeHUDNumbers ()
+        {
+
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        public void ShowHUD ()
+        {
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        public void HideHUD ()
+        {
+        }
+
+        /* ================================================================================================================================== */
+        // HUD NUMBER
+        /* ================================================================================================================================== */
+
+        // TODO: Maybe instead of this struct make a child of SpriteNumber with Type?
+        [Serializable] 
+        public struct HUDNumber
+        {
+            public Type         _Type;
+            public SpriteNumber SpriteNumber;
+
+            public enum Type
+            {
+                Level,
+                Score,
+                Lives,
+                Health,
+                Ammo
+            }
+        }
+    }
+}
