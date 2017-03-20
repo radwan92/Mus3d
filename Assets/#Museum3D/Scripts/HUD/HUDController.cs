b52ad822cde0945aab63ce91da091730a5f0d0a1
Filip Radwaniecki <filip.radwaniecki@gmail.com>
2017-03-20 21:39:00 +0100

    Finished HUD initialization and values display

--- Assets/#Museum3D/Scripts/HUD/HUDController.cs
+++ Assets/#Museum3D/Scripts/HUD/HUDController.cs
@@ -1,16 +1,61 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mus3d
 {
     public class HUDController : MonoBehaviour
     {
-        [SerializeField] HUDNumber[] m_hudNubmers;
+        [SerializeField] SpriteRenderer m_weaponSprite;
+        [SerializeField] HUDNumber[]    m_hudNumbers;
+
+        Dictionary<HUDNumber.Type, HUDNumber> m_hudNumbersByType;
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void Start ()
+        {
+            InitializeHUDNumbers ();
+        }
 
         /* ---------------------------------------------------------------------------------------------------------------------------------- */
         void InitializeHUDNumbers ()
         {
+            m_hudNumbersByType = new Dictionary<HUDNumber.Type, HUDNumber> (10);
+            for (int i = 0; i < m_hudNumbers.Length; i++)
+            {
+                var hudNumber = m_hudNumbers[i];
+                m_hudNumbersByType.Add (hudNumber._Type, hudNumber);
+                SetupHUDNumber (hudNumber);
+            }
+        }
 
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void SetupHUDNumber (HUDNumber hudNumber)
+        {
+            switch (hudNumber._Type)
+            {
+                case HUDNumber.Type.Score:
+                    Player.E_ScoreChanged += () => hudNumber.Set (Player.Score);
+                    hudNumber.Set (Player.Score);
+                    break;
+                case HUDNumber.Type.Lives:
+                    Player.E_LivesChanged += () => hudNumber.Set (Player.Lives);
+                    hudNumber.Set (Player.Lives);
+                    break;
+                case HUDNumber.Type.Health:
+                    Player.E_HealthChanged += () => hudNumber.Set (Player.CurrentHealth);
+                    hudNumber.Set (Player.CurrentHealth);
+                    break;
+                case HUDNumber.Type.Ammo:
+                    Ammunition.E_AmmoChanged += () => hudNumber.Set (Ammunition.GetCount ());
+                    hudNumber.Set (Ammunition.GetCount ());
+                    break;
+                case HUDNumber.Type.Level:
+                    Player.E_LevelChanged += () => hudNumber.Set (Player.Level);
+                    hudNumber.Set (Player.Level);
+                    break;
+                default:
+                    break;
+            }
         }
 
         /* ---------------------------------------------------------------------------------------------------------------------------------- */
@@ -21,27 +66,6 @@ namespace Mus3d
         /* ---------------------------------------------------------------------------------------------------------------------------------- */
         public void HideHUD ()
         {
-        }
-
-        /* ================================================================================================================================== */
-        // HUD NUMBER
-        /* ================================================================================================================================== */
-
-        // TODO: Maybe instead of this struct make a child of SpriteNumber with Type?
-        [Serializable] 
-        public struct HUDNumber
-        {
-            public Type         _Type;
-            public SpriteNumber SpriteNumber;
-
-            public enum Type
-            {
-                Level,
-                Score,
-                Lives,
-                Health,
-                Ammo
-            }
         }
     }
 }
---
+++ Assets/#Museum3D/Scripts/HUD/HUDNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace Mus3d
+{
+    public class HUDNumber : SpriteNumber
+    {
+        public Type _Type;
+
+        public enum Type
+        {
+            Level,
+            Score,
+            Lives,
+            Health,
+            Ammo
+        }
+    }
+}
