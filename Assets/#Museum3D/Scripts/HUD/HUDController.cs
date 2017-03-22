0d4aa1da072197625e4d14758bda81994d998ee1
Filip Radwaniecki <filip.radwaniecki@gmail.com>
2017-03-22 19:24:20 +0100

    Added HUD toggling and fading

--- Assets/#Museum3D/Scripts/HUD/HUDController.cs
+++ Assets/#Museum3D/Scripts/HUD/HUDController.cs
@@ -1,17 +1,79 @@
 ﻿using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
 
 namespace Mus3d
 {
     public class HUDController : MonoBehaviour
     {
+        [Range (0f, 2f)] [SerializeField] float m_fadeTime;
+
+        SpriteRenderer[] m_hudRenderers;
+
+        DOGetter<Color>[] m_rendererColorGetters;
+        DOSetter<Color>[] m_rendererColorSetter;
+
+        bool m_isShowingHUD;
+
         /* ---------------------------------------------------------------------------------------------------------------------------------- */
-        public void ShowHUD ()
+        void Start ()
         {
+            InitializeHUDRenderers ();
         }
 
         /* ---------------------------------------------------------------------------------------------------------------------------------- */
-        public void HideHUD ()
+        void InitializeHUDRenderers ()
         {
+            m_hudRenderers         = GetComponentsInChildren<SpriteRenderer> ();
+            m_rendererColorGetters = new DOGetter<Color> [m_hudRenderers.Length];
+            m_rendererColorSetter  = new DOSetter<Color> [m_hudRenderers.Length];
+
+            for (int i = 0; i < m_hudRenderers.Length; i++)
+            {
+                int index = i;
+                m_rendererColorGetters[i] = () => m_hudRenderers[index].color;
+                m_rendererColorSetter[i]  = color => m_hudRenderers[index].color = color;
+            }
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void Update ()
+        {
+            if (Inp.GetDown (Inp.Key.Start))
+            {
+                ToggleHUD ();
+            }
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void ToggleHUD ()
+        {
+            if (m_isShowingHUD)
+                HideHUD ();
+            else
+                ShowHUD ();
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void ShowHUD ()
+        {
+            if (m_isShowingHUD)
+                return;
+            m_isShowingHUD = true;
+
+            for (int i = 0; i < m_hudRenderers.Length; i++)
+                DOTween.ToAlpha (m_rendererColorGetters[i], m_rendererColorSetter[i], 1f, m_fadeTime);
+        }
+
+        /* ---------------------------------------------------------------------------------------------------------------------------------- */
+        void HideHUD ()
+        {
+            if (!m_isShowingHUD)
+                return;
+            m_isShowingHUD = false;
+
+            for (int i = 0; i < m_hudRenderers.Length; i++)
+                DOTween.ToAlpha (m_rendererColorGetters[i], m_rendererColorSetter[i], 0f, m_fadeTime);
         }
     }
 }
